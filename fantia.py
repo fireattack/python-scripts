222a89fde9391cad51fa086e6ed898756bab9264
fireattack <human.peng@gmail.com>
2020-11-26 09:42:33 -0600

    Update fantia

--- fantia.py
+++ fantia.py
@@ -4,6 +4,7 @@ import re
 from pathlib import Path
 from urllib.parse import unquote, urljoin
 import concurrent.futures
+from natsort import natsorted
 
 API_POSTS = "https://fantia.jp/api/v1/posts/{}"
 API_FANCLUB = "https://fantia.jp/api/v1/fanclubs/{}" # Not used for now
@@ -11,40 +12,54 @@ HTML_POSTLIST = "https://fantia.jp/fanclubs/{}/posts?page={}"
 
 class FantiaDownloader:
 
-    def __init__(self, fanclub, output, key, skip_existing=True):
+    def __init__(self, fanclub, output, key, skip_existing=True, quick_stop=True):
         super().__init__()
         self.key = key
         self.fanclub = fanclub
         self.output = Path(output)
         self.skip_existing = skip_existing
+        self.quick_stop = skip_existing and quick_stop
 
     def fetch(self, url):
         return requests.get(url, cookies={"_session_id": self.key})
 
     def downloadAll(self):
+        print(f'Fanclub {self.fanclub}: download all to {self.output}.')
         if not self.output.exists():
             self.output.exists.mkdir(parents=True, exist_ok=True)
         existing_ids = [m[1] for f in self.output.iterdir() if (m := re.match(r'(\d+) *', f.name))]
         existing_ids = list(dict.fromkeys(existing_ids))
         print(f'{len(existing_ids)} IDs have already been downloaded.')
 
-        page = 1
-        results = []
-
-        while True:
-            print(f"Attempting to fetch page {page}...")
-            out = self.fetchGalleryPage(page)
-            if out:
-                results.extend(out)
-                page += 1
-            else:
-                break
+        def fetchAll():
+            results = []; page = 1
+            while True:
+                print(f"Attempting to fetch page {page}...")
+                out = self.fetchGalleryPage(page)
+                if out:
+                    out = natsorted(out, reverse=True)
+                    for id in out:
+                        if self.quick_stop and id in existing_ids:
+                            print(f'Encountered existing id {id}. Quick stop.')
+                            return results
+                        results.append(id)
+                    page += 1
+                else:
+                    return results
+        results = fetchAll()
         results = list(dict.fromkeys(results))
+        print(f"Get {len(results)} IDs from list.")
+
+        skipped = []; to_be_dl = []
+        for id in results:
+            if self.skip_existing and id in existing_ids:
+                skipped.append(id)
+            else:
+                to_be_dl.append(id)
+        if skipped:
+            print(f"Skip {len(skipped)} existing IDs.")
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as ex:
-            for id in results:
-                if self.skip_existing and id in existing_ids:
-                    print(f'{id} is already downloaded. Skip.')
-                    continue
+            for id in to_be_dl:
                 ex.submit(self.getPostPhotos, id)
 
     def fetchGalleryPage(self, page):
@@ -64,7 +79,7 @@ class FantiaDownloader:
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as ex:
             if thumb := d['post'].get('thumb', None):
                 img_url = thumb['original']
-                stem, ext = getWebName(img_url)                
+                stem, ext = getWebName(img_url)
                 ex.submit(download, img_url, filename= self.output / f'{id} !cover.{ext}', verbose=1)
             if post_contents := d['post'].get('post_contents', None):
                 for c in post_contents:
